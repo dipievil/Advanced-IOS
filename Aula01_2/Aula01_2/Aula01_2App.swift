e318628460096fb18540b992c2a2e991aef2a648
Diego Ritzel <dipi.evil@gmail.com>
2021-09-11 15:39:35 -0300

    Aula 01

---
+++ Aula01_2/Aula01_2/Aula01_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula01_2App.swift
+//  Aula01_2
+//
+//  Created by IOS SENAC on 11/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Aula01_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
