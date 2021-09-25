ac9dbaef2c77e1d1658b6201906218d7d17d8a2e
Diego Ritzel <dipi.evil@gmail.com>
2021-09-25 12:14:26 -0300

    Missing files

---
+++ AppOndeEstou/AppOndeEstou/AppOndeEstouApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppOndeEstouApp.swift
+//  AppOndeEstou
+//
+//  Created by IOS SENAC on 11/09/21.
+//
+
+import SwiftUI
+
+@main
+struct AppOndeEstouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
