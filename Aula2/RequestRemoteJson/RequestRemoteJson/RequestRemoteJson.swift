e126e09d983f54abce28c16d70f0f7adaf502b8a
Diego Ritzel <dipi.evil@gmail.com>
2021-09-25 12:15:10 -0300

    Application with Alamofire

---
+++ Aula2/RequestRemoteJson/RequestRemoteJson/RequestRemoteJson.swift
@@ -0,0 +1,17 @@
+//
+//  RequestRemoteJson.swift
+//  Aula01
+//
+//  Created by IOS SENAC on 11/09/21.
+//
+
+import SwiftUI
+
+@main
+struct RequestRemoteJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
